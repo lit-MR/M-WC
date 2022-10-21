841fa9bc775de79a44dbe1485542637dce981271
IDM <mary.ryan@lit.ie>
2022-10-21 14:40:29 +0100

    Create testQuotes.js

---
+++ testQuotes.js
@@ -0,0 +1,25 @@
+[
+  {
+    text: "Aithníonn ciaróg, ciaróg eile.",
+    author: "Unknown"
+  },
+  {
+    text: "Ní dhéanfadh an saol capall rása d’asal.",
+    author: "Unknown"
+  },
+   {
+    text: "Fillean an feall ar an bhfeallaire.",
+    author: "Unknown"
+  },
+   {
+    text: "Tús maith leath na hoibre.",
+    author: "Unknown"
+  },
+   {
+    text: "Níl saoi gan locht.",
+    author: "Unknown"
+  }
+]
+
+
+
